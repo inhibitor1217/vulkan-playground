a7ea914c201c2ff19703b9028690446a3654647a
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-06-05 00:30:39 +0900

    feat: initialize vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -18,29 +18,60 @@ public:
 	}
 
 private:
-	GLFWwindow* window;
+	GLFWwindow* glfwWindow;
+	VkInstance vkInstance;
 
 	void initWindow() {
-		glfwInit();
+		auto result = glfwInit();
+		if (result != GLFW_TRUE) {
+			throw std::runtime_error("Failed to initialize GLFW");
+		}
 
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		glfwWindow = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
 	void initVulkan() {
+		createVulkanInstance();
+	}
 
+	void createVulkanInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions = nullptr;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &vkInstance) != VK_SUCCESS) {
+			throw new std::runtime_error("Failed to create Vulkan instance");
+		}
 	}
 
 	void mainLoop() {
-		while (!glfwWindowShouldClose(window)) {
+		while (!glfwWindowShouldClose(glfwWindow)) {
 			glfwPollEvents();
 		}
 	}
 
 	void cleanup() {
-		glfwDestroyWindow(window);
+		glfwDestroyWindow(glfwWindow);
 
 		glfwTerminate();
 	}
