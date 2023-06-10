2925875a483ae555dc5717e676fa0ebc2a0752e7
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-06-10 20:27:35 +0900

    refactor: ensure memory align of ubos with shaders

--- src/main.cpp
+++ src/main.cpp
@@ -73,9 +73,9 @@ const Mesh mesh = {{{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
                    {0, 1, 2, 2, 3, 0}};
 
 struct UniformBufferObject {
-  glm::mat4 model;
-  glm::mat4 view;
-  glm::mat4 proj;
+  alignas(16) glm::mat4 model;
+  alignas(16) glm::mat4 view;
+  alignas(16) glm::mat4 proj;
 };
 
 class Application {
